141ee994f345ab3d14521dc83c77e25de8573391
Kyle Reiley <kreiley@utexas.edu>
2016-10-17 20:55:13 +0100

    finished localization

--- fixed_beamformer/localization.c
+++ fixed_beamformer/localization.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "localization.h"
 
 #define threshold 0.5
 #define speed_of_sound 343.0
 #define freq 16000
 
-/*assumes a equalateral triangle microphone array because its easier*/
+/*must be triangle microphone array because its easier*/
 
 int buffer_size = 0;
-double distance = 0;
+double distance12 = 0;
+double distance13 = 0;
+double distance23 = 0;
+double angle12 = 0;
+double angle13 = 0;
+double angle21 = 0;
+double angle23 = 0;
+double angle31 = 0;
+double angle32 = 0;
 
-struct microphone {
-	double x;
-	double y;
-	double* buffer;
-} microphone;
-
-
-microphone * localization_init(double x, double y, double * buffer){
+microphone * build_mic(double x, double y, double * buffer){
 	microphone * mic = malloc(sizeof(microphone));
 	mic->x = x;
 	mic->y = y;
@@ -29,7 +31,7 @@ microphone * localization_init(double x, double y, double * buffer){
 
 /*tan((speed of sound * number of samples / freq)/ distance) = angle */ 
 
-int find_source(microphone * m1, microphone * m2, microphone * m3){
+double find_source(microphone * m1, microphone * m2, microphone * m3){
 	bool done1  = false;
 	bool done2  = false;
 	bool done3  = false;
@@ -69,17 +71,52 @@ int find_source(microphone * m1, microphone * m2, microphone * m3){
 		if(third){break;}
 	}
 	if(num_samples1 < 0){return -1;}
-	double reference_angle = atan(((speed_of_sound * num_samples1)/freq)/distance);
+	double delay = (speed_of_sound * num_samples1)/freq;
+	if     (first == 1 && second == 2){return atan(delay/distance12) + angle12;}
+	else if(first == 1 && second == 3){return atan(delay/distance13) + angle13;}
+	else if(first == 2 && second == 1){return atan(delay/distance12) + angle21;}
+	else if(first == 2 && second == 3){return atan(delay/distance23) + angle23;}
+	else if(first == 3 && second == 1){return atan(delay/distance13) + angle31;}
+	else if(first == 3 && second == 2){return atan(delay/distance23) + angle32;}
+	return -1;
 }
 
 void set_buffer_size(int bf_size){
 	buffer_size = bf_size;
 }
 
-void calc_distance(microphone m1, microphone m2){
+double distance_calc(microphone * m1, microphone * m2){
 	double a, b;
 	a = m1->x - m2->x;
 	b = m1->y - m2->y;	
-	distance = sqrt(a*a + b*b);
+	return sqrt(a*a + b*b);
+}
+
+double degree_calc(microphone * m1, microphone *m2){
+	double x , y, m, theta, rho;
+	x = m2->x - m1->x;
+	y = m2->y - m1->y;
+	if(x == 0 && y == 0){printf("error");return -7;}
+	else if(x == 0){rho = M_PI/2;}
+	else if(y == 0){rho = 0;}
+	else {
+		m = y / x;
+		rho = atan(m);
+	}
+	if(x > 0 && y > 0){return rho;}
+	else if (x < 0){return rho + M_PI/2;}
+	else if (x > 0 && y < 0){return rho + 2*M_PI;}
+}
+
+void init_triangle(microphone * m1, microphone * m2, microphone * m3){
+	angle12 = degree_calc(m1, m2);
+	angle13 = degree_calc(m1, m3);
+	angle21 = degree_calc(m2, m1);
+	angle23 = degree_calc(m2, m3);
+	angle31 = degree_calc(m3, m1);
+	angle32 = degree_calc(m3, m2);
+	distance12 = distance_calc(m1, m2);
+	distance13 = distance_calc(m1, m3);
+	distance23 = distance_calc(m2 ,m3); 
 }
 
--- fixed_beamformer/localization.h
+++ fixed_beamformer/localization.h
@@ -2,7 +2,17 @@
 #ifndef __DELAY_H__
 #define __DELAY_H__
 
+typedef struct microphone {
+	double x;
+	double y;
+	double * buffer;
+} microphone;
 
-
+microphone * build_mic(double x, double y, double * buffer);
+double find_source(microphone * m1, microphone * m2, microphone * m3);
+void set_buffer_size(int bf_size);
+double distance_calc(microphone * m1, microphone * m2);
+double degree_calc(microphone * m1, microphone * m2);
+void init_triangle(microphone * m1, microphone * m2, microphone * m3); 
 
 #endif
